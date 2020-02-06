edf2514a205e38d051141555800499b5faf2ede4
adilpratama <adilpratama71@yahoo.com>
2020-02-06 15:22:36 +0700

    add constraint unique on column email user

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,6 +4,9 @@ module.exports = function(err, req, res, next) {
     res.status(400).json(errors);
   } else if (err.name == "JsonWebTokenError") {
     res.status(401).json("Please login first");
+  } else if (err.name == "SequelizeUniqueConstraintError") {
+    const errors = err.errors.map(el => el.message);
+    res.status(400).json(errors);
   } else {
     res.status(err.status || 500).json(err.message || "Internal Server Error");
   }
