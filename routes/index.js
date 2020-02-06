4e87953bc28948aff1f03f035afe3dc87fb404da
adilpratama <adilpratama71@yahoo.com>
2020-02-06 14:16:18 +0700

    add default route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const router = require("express").Router();
 const userRoutes = require("./user");
 const todoRoutes = require("./todo");
 
+router.get("/", function(req, res) {
+  res.status(200).json({
+    message: "Welcome to fancy-todo API"
+  });
+});
 router.use("/users", userRoutes);
 router.use("/todos", todoRoutes);
 module.exports = router;
