4982cd7d86e50a414e690d2f7cc050a2c10c81d7
adilpratama <adilpratama71@yahoo.com>
2020-02-09 22:10:50 +0700

    change err handler for constraint

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,7 +5,13 @@ module.exports = function(err, req, res, next) {
   } else if (err.name == "JsonWebTokenError") {
     res.status(401).json("Please login first");
   } else if (err.name == "SequelizeUniqueConstraintError") {
-    const errors = err.errors.map(el => el.message);
+    const errors = err.errors.reduce((acc, val) => {
+      if (val.message == "email must be unique") {
+        acc.push("Email has been used");
+        console.log("here");
+      }
+      return acc;
+    }, []);
     res.status(400).json(errors);
   } else {
     res.status(err.status || 500).json(err.message || "Internal Server Error");
